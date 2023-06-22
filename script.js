14529bf64d56d3ac34676f01cd4abdd7968f639e
Wagner Alves Viana <walvesvi@emeal.nttdata.com>
2023-06-22 19:54:18 -0300

    criação portfolio

---
+++ script.js
@@ -0,0 +1,26 @@
+// efeito de digitando
+const title = document.querySelector(".title-h2");
+activeEffect(title);
+
+function activeEffect(elemento) {
+  const arrTexto = elemento.innerHTML.split("");
+  elemento.innerHTML = "";
+  arrTexto.forEach((letra, i) => {
+    setTimeout(() => {
+      elemento.innerHTML += letra;
+    }, 75 * i);
+  });
+}
+
+
+const activeMenu = document.querySelector(".fa-solid");
+const navMenu = document.querySelector(".navigation");
+activecionMenu();
+
+function activecionMenu() {
+ 
+  activeMenu.addEventListener("click", () => { 
+    activeMenu.classList.toggle("fa-x");
+    navMenu.classList.toggle("active");
+  });
+}
